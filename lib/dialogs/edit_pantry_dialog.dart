6d69da7d24d247839a25e4eddf932d39344a7e09
Jan Porcher <jm.porcher1@gmail.com>
2023-07-31 22:55:47 +0200

    HOME. Made Edit Pantry Dialog stateless by using ValueNotifier. Plan to do the same with all dialogs.

--- lib/dialogs/edit_pantry_dialog.dart
+++ lib/dialogs/edit_pantry_dialog.dart
@@ -3,91 +3,73 @@ import '../models/pantry.dart';
 import '../providers/pantry_list_provider.dart';
 import 'package:provider/provider.dart';
 
-class EditPantryDialog extends StatefulWidget {
-  const EditPantryDialog({required this.pantry, super.key});
-
+class EditPantryDialog extends StatelessWidget {
+  EditPantryDialog({required this.pantry, super.key});
   final Pantry pantry;
-
-  @override
-  State<EditPantryDialog> createState() => _EditPantryDialogState();
-}
-
-class _EditPantryDialogState extends State<EditPantryDialog> {
-  bool itemInStock = false;
-  String newTitle = '';
+  final bool itemInStock = false;
+  final _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    final Pantry pantry = widget.pantry;
-    final title = Provider.of<
+    ValueNotifier<String> textNotifier = ValueNotifier<String>(pantry.pantryTitle);
+    _controller.text = pantry.pantryTitle;
+    textNotifier.addListener(() => _controller.text = textNotifier.value);
 
     return AlertDialog(
-      title: const Text('Edit Pantry name:'),
-      content: ChangeNotifierProvider<ValueNotifier<String>>(
-        create: (_) => ValueNotifier(pantry.pantryTitle),
-        child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                padding: const EdgeInsets.all(16),
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 3,
-                      child: TextFormField(
-                        initialValue: pantry.pantryTitle,
-                        autofocus: true,
-                        onChanged: (newString) {
-                          setState(() {
-                            newTitle = newString;
-                          });
-                        },
-                      ),
+        title: const Text('Edit Pantry name:'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: TextFormField(
+                      controller: _controller,
+                      autofocus: true,
                     ),
-                    Expanded(
-                      flex: 1,
-                      child: TextButton(
-                          onPressed: () {
-                            if (newTitle.isNotEmpty) {
-                              context
-                                  .read<PantryProvider>()
-                                  .renamePantry(pantry, newTitle);
-                              Navigator.pop(context);
-                            }
-                          },
-                          child: const Icon(Icons.save)),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 50),
-              GestureDetector(
-                  onTap: () {
-                    context.read<PantryProvider>().removePantry(pantry);
-                    Navigator.pop(context);
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.all(16),
-                    color: const Color(0x5BAAD9FF),
-                    child: const Row(
-                        children: [
-                        Expanded(flex: 3, child: Text('Delete Pantry')),
-                    Expanded(flex: 1, child: Icon(Icons.delete)),
-            ]
-        ),
                   ),
-    ),
-    ],
-    ),
-      ),
-    actions: [
-    TextButton(
-    onPressed: () {
-    Navigator.pop(context);
-    },
-    child: const Text('Cancel'))
-    ,
-    ]
-    );
+                  Expanded(
+                    flex: 1,
+                    child: TextButton(
+                        onPressed: () {
+                          if (_controller.text.isNotEmpty) {
+                            context
+                                .read<PantryProvider>()
+                                .renamePantry(pantry, _controller.text);
+                            Navigator.pop(context);
+                          }
+                        },
+                        child: const Icon(Icons.save)),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 50),
+            GestureDetector(
+              onTap: () {
+                context.read<PantryProvider>().removePantry(pantry);
+                Navigator.pop(context);
+              },
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                color: const Color(0x5BAAD9FF),
+                child: Row(children: const [
+                  Expanded(flex: 3, child: Text('Delete Pantry')),
+                  Expanded(flex: 1, child: Icon(Icons.delete)),
+                ]),
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Cancel')),
+        ]);
   }
 }
