5bb4de12a5b6bd6538e0d60a74b88ffce6bf3c9b
janmartinporcher <jm.porcher1@gmail.com>
2023-08-07 14:02:52 +0200

    OFFICE. Minor code optimization on the welcome screen stack.

--- lib/widgets/pantry_screen.dart
+++ lib/widgets/pantry_screen.dart
@@ -28,64 +28,57 @@ class PantryScreen extends StatelessWidget {
           });
     } else {
       return Scaffold(
-        body: SafeArea(
-          child: Container(
-            color: Colors.blue,
-            child: Center(
-              child: Stack(
-                children: [
-                  Center(
+        body: Container(
+          color: Colors.blue,
+          child: Center(
+            child: Stack(
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(32),
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(32),
+                      child: SvgPicture.asset(
+                        'assets/pantry_welcome.svg',
+                        semanticsLabel: 'Food pantry',
+                      ),
+                    ),
+                  ),
+                ),//Image container
+                Positioned.fill(
+                  child: MaterialButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AddPantryDialog(),
+                      );
+                    },
                     child: Container(
-                      margin: const EdgeInsets.all(32),
-                      child: AspectRatio(
-                        aspectRatio: 1, // Maintain aspect ratio (1:1 for square)
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(32),
-                          child: SvgPicture.asset(
-                            'assets/pantry_welcome.svg',
-                            semanticsLabel: 'Food pantry',
-                            fit: BoxFit.cover,
-                          ),
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: const [
+                          BoxShadow(
+                            offset: Offset(3, 3),
+                            blurRadius: 5,
+                            color: Colors.black26,
+                          )
+                        ],
+                      ),
+                      child: const Text(
+                        'Add your first pantry',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
                       ),
                     ),
                   ),
-                  Positioned.fill(
-                    child: Center(
-                      child: MaterialButton(
-                        onPressed: () {
-                          showDialog(
-                            context: context,
-                            builder: (context) => AddPantryDialog(),
-                          );
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.all(16),
-                          decoration: BoxDecoration(
-                            color: Colors.grey,
-                            borderRadius: BorderRadius.circular(8),
-                            boxShadow: const [
-                              BoxShadow(
-                                offset: Offset(3, 3),
-                                blurRadius: 5,
-                                color: Colors.black26,
-                              )
-                            ],
-                          ),
-                          child: const Text(
-                            'Add your first pantry',
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                ),//Add Pantry button
+              ],
             ),
           ),
         ),
