6f8ca3c0f79f1d81e0cd9bd36b288a3cc7a31fc9
Jan Porcher <jm.porcher1@gmail.com>
2023-08-09 15:35:18 +0200

    HOME. Honestly, not sure.

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthProvider extends ChangeNotifier {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  User? _user;
+
+  // Constructor
+  AuthProvider() {
+    _firebaseAuth.authStateChanges().listen((User? user) {
+      _user = user;
+      notifyListeners(); // Notify listeners when authentication state changes
+    });
+  }
+
+  // Getter to access the current user
+  User? get user => _user;
+
+  // Sign up with email and password
+  Future<void> signUpWithEmail(String email, String password) async {
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  // Sign in with email and password
+  Future<void> signInWithEmail(String email, String password) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  // Sign out
+  Future<void> signOut() async {
+    try {
+      await _firebaseAuth.signOut();
+    } catch (error) {
+      rethrow;
+    }
+  }
+}
