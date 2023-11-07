45ce658c224a88569e295e9300dc18751aed87f1
Jan Porcher <jm.porcher1@gmail.com>
2023-11-07 22:42:33 +0100

    HOME. Fixed dimensions of quickadd_view to match the listtiles above.

--- lib/widgets/quickadd_view.dart
+++ lib/widgets/quickadd_view.dart
@@ -33,48 +33,58 @@ class _ShoppingItemQuickAddState extends State<ShoppingItemQuickAdd> {
 
     ValueNotifier<bool> fieldIsEmpty = ValueNotifier(true);
 
-    return ListTile(
-      tileColor: backgroundColor,
-      leading: SizedBox(
-        width: 230,
-        child: TextField(
-          onChanged: (_) {
-            if (textEditingController.text.isNotEmpty) {
-              fieldIsEmpty.value = false;
-            } else {
-              fieldIsEmpty.value = true;
-            }
-          },
-          style: const TextStyle(fontSize: 14),
-          textAlign: TextAlign.center,
-          inputFormatters: [LengthLimitingTextInputFormatter(90)],
-          controller: textEditingController,
-          decoration: const InputDecoration(
-            hintText: '(Add item)',
-            hintStyle: TextStyle(color: Colors.black26, fontSize: 14),
-            focusedBorder: UnderlineInputBorder(
-              borderSide: BorderSide(color: Colors.black12, width: 3)
-            )
+    return SizedBox(
+      height: 40,
+      child: ListTile(
+        tileColor: backgroundColor,
+        dense: true,
+        leading: Container(
+          width: 230,
+          height: 40,
+          padding: const EdgeInsets.only(top: 8, bottom: 8),
+          child: TextField(
+            onChanged: (_) {
+              if (textEditingController.text.isNotEmpty) {
+                fieldIsEmpty.value = false;
+              } else {
+                fieldIsEmpty.value = true;
+              }
+            },
+            style: const TextStyle(fontSize: 14),
+            textAlign: TextAlign.start,
+            inputFormatters: [LengthLimitingTextInputFormatter(90)],
+            controller: textEditingController,
+            decoration: const InputDecoration(
+              hintText: '(Add item)',
+              hintStyle: TextStyle(color: Colors.black26, fontSize: 14),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.black12, width: 3)
+              ),
+            ),
           ),
         ),
-      ),
-      trailing: SizedBox(
-        width: 40,
-        child: ValueListenableBuilder(
-          valueListenable: fieldIsEmpty,
-          builder: (BuildContext context, bool value, Widget? child) {
-            return ElevatedButton(
-                style:
-                ButtonStyle(
-                    backgroundColor: fieldIsEmpty.value ? MaterialStateProperty.all(kColor7) : MaterialStateProperty.all(kColor51),
-                    padding: MaterialStateProperty.all(const EdgeInsets.all(0))),
-                onPressed: () => showItemQuickAddDialog(),
-                child: const Icon(
-                  Icons.add,
-                  color: kColor11,
-                ));
-          },
+        trailing: Padding(
+          padding: const EdgeInsets.only(left: 2.0, top: 2, right: 9, bottom: 2),
+          child: SizedBox(
+            width: 30,
+            height: 30,
+            child: ValueListenableBuilder(
+              valueListenable: fieldIsEmpty,
+              builder: (BuildContext context, bool value, Widget? child) {
+                return ElevatedButton(
+                    style:
+                    ButtonStyle(
+                        backgroundColor: fieldIsEmpty.value ? MaterialStateProperty.all(kColor7) : MaterialStateProperty.all(kColor51),
+                        padding: MaterialStateProperty.all(const EdgeInsets.all(0))),
+                    onPressed: () => showItemQuickAddDialog(),
+                    child: const Icon(
+                      Icons.add,
+                      color: kColor1,
+                    ));
+              },
 
+            ),
+          ),
         ),
       ),
     );
