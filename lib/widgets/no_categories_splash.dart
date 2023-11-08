d871afbe40a4f4c9d3ef17dd5655d9f51081d0c2
Jan Martin Porcher <janmartinporcher@Mac-mini-von-Jan.local>
2023-11-08 19:36:38 +0100

    OFFICE. Bottom overflow in new pantry is fixed.

--- lib/widgets/no_categories_splash.dart
+++ lib/widgets/no_categories_splash.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:shared_pantry/widgets/sp_cards.dart';
 
 class NoCategoriesSplashScreen extends StatelessWidget {
   const NoCategoriesSplashScreen({super.key});
@@ -8,36 +7,38 @@ class NoCategoriesSplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Stack(children: [
-        Container(
-          margin: const EdgeInsets.all(32),
-          child: AspectRatio(
-            aspectRatio: 1,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(32),
-              child: SvgPicture.asset(
-                'assets/pantry_welcome.svg',
-                semanticsLabel: 'Food pantry',
+    return Expanded(
+      child: Center(
+        child: Stack(children: [
+          Container(
+            margin: const EdgeInsets.all(32),
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(32),
+                child: SvgPicture.asset(
+                  'assets/pantry_welcome.svg',
+                  semanticsLabel: 'Food pantry',
+                ),
               ),
             ),
-          ),
-        ), //Image container
-        const Positioned(
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0,
-          child: Center(
-            child: Card(
-              child: Padding(
-                padding: EdgeInsets.all(24.0),
-                child: Text('This pantry has no categories yet.'),
+          ), //Image container
+          const Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            child: Center(
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(24.0),
+                  child: Text('This pantry has no categories yet.'),
+                ),
               ),
             ),
-          ),
-        )
-      ]),
+          )
+        ]),
+      ),
     );
   }
 }
